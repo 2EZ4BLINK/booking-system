1bedc0530246f67a17378dcf0f3872a35a3b353a
2EZ4BLINK <blinkcode24@gmail.com>
2026-06-03 15:23:43 +0800

    update: added props and used css function

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import { Button } from "./ui/Button";
 import { Input } from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import { Heading } from "./ui/Heading";
 
 const StyledApp = styled.main`
   background-color: orangered;
@@ -19,9 +15,13 @@ const App = () => {
       <GlobalStyles />
 
       <StyledApp>
-        <H1>The wild oasis</H1>
+        <Heading as="h1">The wild oasis</Heading>
+
+        <Heading as="h2">Checking</Heading>
         <Button>Check in</Button>
         <Button>Check out</Button>
+
+        <Heading as="h3">Form</Heading>
         <Input type="number" placeholder="Number of guests" />
       </StyledApp>
     </>
