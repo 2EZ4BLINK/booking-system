178a9d2c63dd11f0f5d03d53df184f120234c159
2EZ4BLINK <blinkcode24@gmail.com>
2026-06-06 13:14:59 +0800

    update: created a data from supabase and connect in on this project

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(() => {
+    const cabinData = async () => {
+      const data = await getCabins();
+      console.log("data", data);
+      return data;
+    };
+
+    cabinData();
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
