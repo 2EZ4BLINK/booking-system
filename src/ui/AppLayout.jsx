bbfa25471029deb6fe1eb31cc1f30294e495deee
2EZ4BLINK <blinkcode24@gmail.com>
2026-06-08 13:21:51 +0800

    update: added toast notification

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -15,6 +15,14 @@ const StyledAppLayout = styled.div`
   height: 100dvh;
 `;
 
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 const AppLayout = () => {
   return (
     <StyledAppLayout>
@@ -22,7 +30,9 @@ const AppLayout = () => {
       <Sidebar />
 
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
